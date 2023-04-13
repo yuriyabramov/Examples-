9998ddf2f5606ed269627a304e73aef0d46e5622
Yury_Lomatov <yury.lomatov@yandex.ru>
2023-04-13 13:06:29 +0300

    add example001

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("2 Hello, World!");
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("2 Hello, World!");
