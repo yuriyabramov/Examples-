976336de02a28a28417a1c6522f2f3e730436076
Yury_Lomatov <yury.lomatov@yandex.ru>
2023-04-13 14:31:26 +0300

    add new info in Example004

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,6 @@
-﻿double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+﻿double numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+double result = numberA / numberB;
+Console.WriteLine(result);
