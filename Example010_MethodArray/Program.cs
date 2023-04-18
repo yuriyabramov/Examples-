24bdcd4df0fd4d3b953e3559697902adcbdf68b0
Yury_Lomatov <yury.lomatov@yandex.ru>
2023-04-18 19:19:46 +0300

    added Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 12, 31, 4, 15, 88, 16, 17, 88};
+
+int n = array.Length;
+int find = 88;
+
+int index = 0;
+
+while (index < n )
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
